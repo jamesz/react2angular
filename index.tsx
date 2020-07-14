650c5b21da6560c8ce1fcaae62148b8343863c03
James Z <james.zhuo@gmail.com>
2020-07-14 13:58:50 +1000

    added options to inject applyAsync and evalAsync, not tested yet

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
-import { IAugmentedJQuery, IComponentOptions } from 'angular'
+import { IAugmentedJQuery, IComponentOptions, IRootScopeService } from 'angular'
 import fromPairs = require('lodash.frompairs')
+import isFunction = require('lodash.isfunction')
 import NgComponent from 'ngcomponent'
 import * as React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
@@ -18,7 +19,8 @@ import { render, unmountComponentAtNode } from 'react-dom'
 export function react2angular<Props>(
   Class: React.ComponentType<Props>,
   bindingNames: (keyof Props)[] | null = null,
-  injectNames: string[] = []
+  injectNames: string[] = [],
+  options: Options | undefined = {}
 ): IComponentOptions {
   const names = bindingNames
     || (Class.propTypes && Object.keys(Class.propTypes) as (keyof Props)[])
@@ -26,23 +28,27 @@ export function react2angular<Props>(
 
   return {
     bindings: fromPairs(names.map(_ => [_, '<'])),
-    controller: ['$element', ...injectNames, class extends NgComponent<Props> {
+    controller: ['$element', '$scope', ...injectNames, class extends NgComponent<Props> {
       static get $$ngIsClass() {
         return true
       }
       isDestroyed = false
       injectedProps: { [name: string]: any }
-      constructor(private $element: IAugmentedJQuery, ...injectedProps: any[]) {
+      constructor(private $element: IAugmentedJQuery, $scope: IRootScopeService, ...injectedProps: any[]) {
         super()
-        this.injectedProps = {}
+        this.injectedProps = { $scope}
         injectNames.forEach((name, i) => {
           this.injectedProps[name] = injectedProps[i]
         })
       }
       render() {
         if (!this.isDestroyed) {
+          let props = this.props
+          if (options && (options.evalAsync || options.applyAsync)) {
+            props = wrapFunctionPropsToForceNgDigestCycle(this.injectedProps.$scope, this.props, options)
+          }
           render(
-            <Class {...this.props} {...this.injectedProps as any} />,
+            <Class {...props} {...this.injectedProps as any} />,
             this.$element[0]
           )
         }
@@ -53,4 +59,31 @@ export function react2angular<Props>(
       }
     }]
   }
+}
+
+interface Options {
+  evalAsync?: boolean
+  applyAsync?: boolean
+}
+
+type Props = {
+  [key: string]: any
+}
+
+function wrapFunctionPropsToForceNgDigestCycle ($scope: IRootScopeService, props: Props, options: Options) : object {
+  return Object.keys(props)
+    .reduce((wrappedProps : object, name: string) => {
+      const prop = props[name]
+      return {
+        ...wrappedProps,
+        [name] : isFunction(prop) ? (...args: any[]) => {
+          prop(...args)
+          if (options.evalAsync) {
+            $scope.$evalAsync()
+          } else if (options.applyAsync) {
+            $scope.$applyAsync()
+          }
+        } : prop
+      }
+    }, {})
 }
